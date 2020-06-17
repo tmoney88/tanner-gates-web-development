bb4bade237c8a8310bc85049243b72c3387960a7
Tanner Gates <tgates4@dmail.dixie.edu>
2020-06-16 21:39:48 -0600

    Change hero image, change background and primary colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,21 @@ import "../App.css";
 
 function Home() {
   return (
-    <div className="hero container">
-      <h1>Let's Build Something</h1>
+    <div className="container">
+      <div className="hero">
+        <h1>Let's Build Something</h1>
+      </div>
+      <h2>Skills</h2>
+      <div className="card-container">
+        <div>HTML</div>
+        <div>CSS</div>
+        <div>SASS</div>
+        <div>JavaScript</div>
+        <div>React.js</div>
+        <div>Node.js</div>
+        <div>Python</div>
+        <div>Django</div>
+      </div>
     </div>
   );
 }
