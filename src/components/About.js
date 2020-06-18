4583231f99241cce3690b54e74e5571b4542bd98
Tanner Gates <tgates4@dmail.dixie.edu>
2020-06-18 08:23:33 -0600

    Add image to About component set up flex-box

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,31 @@
 import React from "react";
+import aboutImg from "../about.png";
 
 function About() {
   return (
-    <div>
-      <h1>About Page works</h1>
+    <div className="About container">
+      <h1>About ME</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <img src={aboutImg} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
     </div>
   );
 }
